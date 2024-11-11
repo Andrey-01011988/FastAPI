15b11ce9a76bb6be02b2596bcd2f868978942019
uservm <example@mail.com>
2024-11-11 16:17:27 +0300

    Исправил замечания flake8

--- app/entry_point_26.py
+++ app/entry_point_26.py
@@ -10,6 +10,6 @@ async def hello():
 # (venv) uservm@uservm-VirtualBox:~/PycharmProjects/python_advanced/module_26_fastapi$ uvicorn homework.entry_point_26:app_26 --reload
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     uvicorn.run(app_26, host='0.0.0.0', port=8000)
 # Запускается либо из консоли либо, если раскомментировать, из файла
